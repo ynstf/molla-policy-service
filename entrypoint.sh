7ecc2a5dc5c661c2f6a106c8d709aed9ceb0580d
YONES <107154559+ynstf@users.noreply.github.com>
2023-11-30 13:00:21 +0100

    fix enterypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APP_PORT=${PORT:-9999}
 cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm auth.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm policy.wsgi:application --bind "0.0.0.0:${APP_PORT}"
